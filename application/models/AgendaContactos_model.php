c970895027fa1501a4a8dceab0b651a1feaafa98
Apotheosus <apotheosus@outlook.com>
2019-07-19 18:08:02 -0300

    model

--- application/models/AgendaContactos_model.php
+++ application/models/AgendaContactos_model.php
@@ -5,15 +5,16 @@ class AgendaContactos_model extends CI_Model
     {
         parent::_coontruct();
     }
- /*   
     public function alta($data){
-        return $this->db->insert('usuario',$data);      
+        return $this->db->insert('contacto',$data);      
     }
     
     public function modifica($data,$id){
-        $this->db->where('usuario.id',$id);
-        return $this->db->update('usuario',$data);     
+        $this->db->select('contacto.*');
+        $this->db->where('contacto.id',$id);
+        return $this->db->update('contacto',$data);     
     }
+ /*   
 
 //Get Usuario
     public function obtener($id){    
@@ -24,10 +25,21 @@ class AgendaContactos_model extends CI_Model
         return $consulta->row();
     }
 */
+
+    public function baja_x_id($id){
+        $this->db->select('contacto.*');
+        $this->db->from('contacto');    
+        $this->db->where('contacto.id',$id);
+        $consulta = $this->db->get();           
+        return $consulta->row();
+    }
+
+
+
     public function listaContactos(){
-     $result =mysqli_query("SELECT * FROM contacto");
+       $result =mysqli_query("SELECT * FROM contacto");
       //  $data   =array();
-     if(mysql_num_rows($result) > 0){
+       if(mysql_num_rows($result) > 0){
         while($row = mysql_fetch_assoc($result)){
             echo $row['id'];
           //$data[] = $row;
