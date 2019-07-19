50ebad79dbe03f76b541ca28b42d8ab351e39b72
Lucia-Gomez327 <luciagomez964@yahoo.com>
2019-07-19 20:23:53 -0300

    model

--- application/models/AgendaContactos_model.php
+++ application/models/AgendaContactos_model.php
@@ -5,24 +5,24 @@ class AgendaContactos_model extends CI_Model
     {
         parent::_coontruct();
     }
-
+//agregar un contacto
     public function alta($data){
         return $this->db->insert('contacto',$data);      
     }
-    
+ //editar un contacto   
     public function modifica($data,$id){
         $this->db->select('contacto.*');
         $this->db->where('contacto.id',$id);
         return $this->db->update('contacto',$data);     
     }
-
+//eliminacion
     public function baja_x_id($id){
         $this->db->select('contacto.*');
         $this->db->from('contacto');    
         $this->db->where('contacto.id',$id);
         $this->db->update('contacto.estado',2);     
     }
-
+//lista de todos los contactos
     public function listaContactos(){
 
         $this->db->select('contacto.*');
@@ -30,7 +30,7 @@ class AgendaContactos_model extends CI_Model
         $query = $this->db->get();
         return $query->result();
     }
-
+//listar o buscar por nombre
     public function obtener_x_nombre($nombre)
     {
         $this->db->select('contacto.*');
