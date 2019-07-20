34cb5271970702083928d53d432313f82a7aeaa2
Lucia-Gomez327 <luciagomez964@yahoo.com>
2019-07-19 21:04:17 -0300

    modelo y controlador

--- application/controllers/AgendaContactos.php
+++ application/controllers/AgendaContactos.php
@@ -13,6 +13,96 @@ class AgendaContactos extends CI_Controller {
     $this->load->view('/footer/footer');
   }
 
+//agregar contactos
+  public function agregar()
+  {
+    $nombre   = $this->input->post('nombre');
+    $apellido = $this->input->post('apellido');
+    $edad     = $this->input->post('edad');
+    $email   = $this->input->post('emali');
+    $telefono = $this->input->post('telefono');
+    $foto =  $this->input->post('foto');
+
+    $data = array(
+      'nombre'   => $nombre,
+      'apellido' => $apellido,
+      'edad' => $edad,
+      'email' => $email,
+      'telefono' => $telefono,
+      'foto' => $foto  
+    );
+
+    $consulta = $this->AgendaContactos_model->buscar_x_email($data->email);
+    if(!$consulta){
+     //alta
+      $this->agenda_model->agregar($data);
+      return true;
+    }
+    else
+    {
+      return true;
+    }
+  }
+
+  //baja
+  public function baja($email)
+  {
+      $nombre = $this->AgendaContactos_model->baja_x_mail($email);
+      redirect('inicio')
+
+  } 
+
+//editar o modificar un contacto
+public function modificar ()
+{
+    $nombre   = $this->input->post('nombre');
+    $apellido = $this->input->post('apellido');
+    $edad     = $this->input->post('edad');
+    $email   = $this->input->post('emali');
+    $telefono = $this->input->post('telefono');
+    $foto =  $this->input->post('foto');
+
+    $data = array(
+      'nombre'   => $nombre,
+      'apellido' => $apellido,
+      'edad' => $edad,
+      'email' => $email,
+      'telefono' => $telefono,
+      'foto' => $foto  
+    );
+
+$consulta = $this->AgendaContactos_model->buscar_x_email($data->email);
+    if(!$consulta){
+     //alta
+      $this->AgendaContactos_model->modifica($data);
+      return true;
+    }
+    else
+    {
+      return true;
+    }
+}
+
+//buscar contactos
+  public function otenerContacto(){
+
+$nombre = $this->input->post('buscar');
+// ↓ Arreglo de contactos
+    $contactos = $this->AgendaContactos_model->obtener_x_nombre($nombre);
+    $lista ='';
+    foreach($contactos as $contacto)
+    {
+     $lista.= $this->load->view('card/contacto_card',$contacto,true);
+   }
+   $data = array(
+    'contactos'=> $lista
+  );
+   return $data;
+ }
+
+
+
+//listar todos los contactos
   public function arrContactos(){
 
 // ↓ Arreglo de contactos
--- application/models/AgendaContactos_model.php
+++ application/models/AgendaContactos_model.php
@@ -6,20 +6,20 @@ class AgendaContactos_model extends CI_Model
         parent::_coontruct();
     }
 //agregar un contacto
-    public function alta($data){
+    public function agregar($data){
         return $this->db->insert('contacto',$data);      
     }
  //editar un contacto   
-    public function modifica($data,$id){
+    public function modifica($data){
         $this->db->select('contacto.*');
-        $this->db->where('contacto.id',$id);
+        $this->db->where('contacto.email',$data->email);
         return $this->db->update('contacto',$data);     
     }
 //eliminacion
-    public function baja_x_id($id){
+    public function baja_x_mail($mail){
         $this->db->select('contacto.*');
         $this->db->from('contacto');    
-        $this->db->where('contacto.id',$id);
+        $this->db->where('contacto.mail',$mail);
         $this->db->update('contacto.estado',2);     
     }
 //lista de todos los contactos
@@ -36,6 +36,15 @@ class AgendaContactos_model extends CI_Model
         $this->db->select('contacto.*');
         $this->db->from('contacto');
         $this->where('contaco.nombre', $nombre);
+        $consulta = $this->db->get();           
+        return $consulta->result();
+    }
+
+     public function obtener_x_email($email)
+    {
+        $this->db->select('contacto.*');
+        $this->db->from('contacto');
+        $this->where('contacto.email', $email);
         $consulta = $this->db->get();           
         return $consulta->result();
     }
